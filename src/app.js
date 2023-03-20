2d6fe6d630b201c6c8981f651b566acfa99381b0
NVR-2023 <nunoviolanterodrigues@hotmail.com>
2023-03-20 21:34:59 +0000

    Added button, general restructuring of onLoad function

--- src/app.js
+++ src/app.js
@@ -5,31 +5,37 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = () => {
-  //write your code here
-  let who = ["The dog", "My grandma", "His turtle", "My bird"];
-  let action = ["ate", "peed", "crushed", "broke"];
-  let what = ["my homework", "the keys", "the car"];
-  let when = [
-    "before the class",
-    "right on time",
-    "when I finished",
-    "during my lunch",
-    "while I was praying"
-  ];
+//Initializing data//
+const WHO = ["The dog", "My grandma", "His turtle", "My bird"];
+const ACTION = ["ate", "peed", "crushed", "broke"];
+const WHAT = ["my homework", "the keys", "the car"];
+const WHEN = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
 
-  const getRandomIndexOfArray = anArray =>
-    Math.floor(Math.random() * anArray.length);
-  const getRandomItemOfArray = anArray =>
-    anArray[getRandomIndexOfArray(anArray)];
-  const getNewExcuse = () => {
-    return `${getRandomItemOfArray(who)} ${getRandomItemOfArray(
-      action
-    )} ${getRandomItemOfArray(what)} ${getRandomItemOfArray(when)}.`;
-  };
-  const renderNewExcuse = () => {
-    document.querySelector("#excuse").innerHTML = getNewExcuse();
-  };
-
-  renderNewExcuse();
+//Code//
+const getRandomIndexOfArray = anArray =>
+  Math.floor(Math.random() * anArray.length);
+const getRandomItemOfArray = anArray => anArray[getRandomIndexOfArray(anArray)];
+const getNewExcuse = () => {
+  return `${getRandomItemOfArray(WHO)} ${getRandomItemOfArray(
+    ACTION
+  )} ${getRandomItemOfArray(WHAT)} ${getRandomItemOfArray(WHEN)}.`;
 };
+
+//onLoad function
+window.onload = () => {
+  document.querySelector("#excuse").innerHTML = getNewExcuse();
+};
+
+//eventListener for Button
+document
+  .querySelector("button")
+  .addEventListener(
+    "click",
+    () => (document.querySelector("#excuse").innerHTML = getNewExcuse())
+  );
